f8c4cc316e176983bb86d94c4381aad4d561260d
Kashish Grover <kashish@goldcast.io>
2022-07-18 14:29:08 +0530

    personalize settings

--- src/settings.h
+++ src/settings.h
@@ -2,15 +2,15 @@
 #define SETTINGS_H
 
 //Weather Settings
-#define CITY_ID "5128581" //New York City https://openweathermap.org/current#cityid
-#define OPENWEATHERMAP_APIKEY "f058fe1cad2afe8e2ddc5d063a64cecb" //use your own API key :)
+#define CITY_ID "1277333" // Bengaluru https://openweathermap.org/current#cityid
+#define OPENWEATHERMAP_APIKEY "2c4caaf6ff8eb7658dc62b63431b3755" //use your own API key :)
 #define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?id=" //open weather api
-#define TEMP_UNIT "metric" //metric = Celsius , imperial = Fahrenheit
+#define TEMP_UNIT "metric" // metric = Celsius , imperial = Fahrenheit
 #define TEMP_LANG "en"
-#define WEATHER_UPDATE_INTERVAL 30 //must be greater than 5, measured in minutes
+#define WEATHER_UPDATE_INTERVAL 30 // must be greater than 5, measured in minutes
 //NTP Settings
 #define NTP_SERVER "pool.ntp.org"
-#define GMT_OFFSET_SEC 3600 * -5 //New York is UTC -5
+#define GMT_OFFSET_SEC 19800 // 3600 x 5.5 for India Standard Time
 #define DST_OFFSET_SEC 3600
 
 watchySettings settings{
